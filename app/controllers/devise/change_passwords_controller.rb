18f1d5023563183a7c459ed3b4a30089f999091e
javagg <lu.lee05@gmail.com>
2012-03-11 02:16:10 +0800

    fix for devise 2.0 and add zh.yml

--- app/controllers/devise/change_passwords_controller.rb
+++ app/controllers/devise/change_passwords_controller.rb
@@ -1,40 +1,35 @@
-class Devise::ChangePasswordsController < ApplicationController
-
-  respond_to :html
-
+class Devise::ChangePasswordsController < DeviseController
+  include Devise::Controllers::Helpers
   prepend_before_filter :authenticate_scope!, :only => [:show, :update]
-  include Devise::Controllers::InternalHelpers
 
   def show
-    render_with_scope :show
+    render :show
   end
 
   def update
-
     if resource.update_with_password(params[resource_name])
-      set_flash_message :notice, :updated
+      set_flash_message :notice, :updated if is_navigational_format?
       respond_with resource, :location => after_update_path_for(resource)
     else
       clean_up_passwords(resource)
-      render_with_scope :show
+      render :show
     end
   end
 
   protected
 
-    def after_update_path_for(resource)
-      if defined?(super)
-        ActiveSupport::Deprecation.warn "Defining after_update_path_for in ApplicationController " <<
-          "is deprecated. Please add a ChangePasswordsController to your application and define it there."
-        super
-      else
-        after_sign_in_path_for(resource)
-      end
+  def after_update_path_for(resource)
+    if defined?(super)
+      ActiveSupport::Deprecation.warn "Defining after_update_path_for in ApplicationController " <<
+                                          "is deprecated. Please add a ChangePasswordsController to your application and define it there."
+      super
+    else
+      after_sign_in_path_for(resource)
     end
+  end
 
-    def authenticate_scope!
-      send(:"authenticate_#{resource_name}!")
-      self.resource = send(:"current_#{resource_name}")
-    end
-
+  def authenticate_scope!
+    send(:"authenticate_#{resource_name}!", :force => true)
+    self.resource = send(:"current_#{resource_name}")
+  end
 end
--- lib/devise_password_changable/routes.rb
+++ lib/devise_password_changable/routes.rb
@@ -1,11 +1,8 @@
 module ActionDispatch::Routing
   class Mapper
-
     protected
-
-      def devise_password_change(mapping, controllers)
-        resource :password_change, :only => [:show, :update], :path => mapping.path_names[:password_change], :controller => controllers[:change_passwords]
-      end
-
+    def devise_password_change(mapping, controllers)
+      resource :password_change, :only => [:show, :update], :path => mapping.path_names[:password_change], :controller => controllers[:change_passwords]
+    end
   end
 end
